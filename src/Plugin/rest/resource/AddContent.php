83f5c687ac3ae6c828f442f6db17f40508950908
Saptaparnee Chaudhuri <saptaparnee8@gmail.com>
2019-09-12 16:18:51 +0530

    The source id parameter is removed from the Body of the post request in AddContent.php
    	Content can be added to multiple sources now.

--- src/Plugin/rest/resource/AddContent.php
+++ src/Plugin/rest/resource/AddContent.php
@@ -44,117 +44,118 @@ class AddContent extends ResourceBase {
 
     else {
       for ($i = 0; $i < count($arg); $i++) {
-        if (!isset($arg[$i]['sourceid'])) {
-          $message = [
-            'success' => 0,
-            'message' => 'sourceid required',
-          ];
-          $statuscode = 400;
-        }
-        else {
-          $source_present = db_query("SELECT COUNT(*) FROM `heritage_source_info` WHERE id = :sourceid", [':sourceid' => $arg[$i]['sourceid']])->fetchField();
-          if ($source_present == 1) {
 
-            $field_name = 'field_' . 'gita' . '_' . $arg[$i]['sourceid'] . '_text';
+        // Loop through the args to extract the sourceid from field name in the post body.
+        foreach ($arg[$i] as $key => $value) {
+          $parameter_info = explode('_', $key);
 
-            if (!isset($arg[$i][$field_name])) {
-              $message = [
-                'success' => 0,
-                'message' => 'required parameters missing(incorrect field name)',
-              ];
-              $statuscode = 400;
-              break;
+          if ($parameter_info[0] == 'field') {
+            $sourceid = $parameter_info[2];
 
-            }
-            else {
-              if (!isset($arg[$i]['positional_index']) || !isset($arg[$i]['language'])) {
+            $source_present = db_query("SELECT COUNT(*) FROM `heritage_source_info` WHERE id = :sourceid", [':sourceid' => $sourceid])->fetchField();
+            if ($source_present == 1) {
 
+              $field_name = 'field_' . $textname . '_' . $sourceid . '_' . $parameter_info[3];
+
+              if (!isset($arg[$i][$field_name])) {
                 $message = [
                   'success' => 0,
-                  'message' => 'required parameters missing(incorrect arguments)',
+                  'message' => 'required parameters missing(incorrect field name)',
                 ];
                 $statuscode = 400;
                 break;
 
               }
-
               else {
-
-                // Convert the language into language code.
-                $languages = \Drupal::service('language_manager')->getLanguages(LanguageInterface::STATE_CONFIGURABLE);
-
-                foreach ($languages as $language) {
-                  if ($arg[$i]['language'] == $language->getName()) {
-                    $arg[$i]['language'] = $language->getId();
-                  }
-                  .
-                }
-
-                // $arg[$i]['language'] = "dv";
-                // Make the db querries here.
-                $textid = db_query("SELECT entity_id FROM `node__field_positional_index` WHERE langcode = :language AND field_positional_index_target_id IN (SELECT entity_id FROM `taxonomy_term__field_position` WHERE field_position_value = :positional_index)", ['language' => $arg[$i]['language'], ':positional_index' => $arg[$i]['positional_index']])->fetchField();
-
-                if (isset($textid) && $textid > 0) {
-                  // Insert the content.
-                  $node = Node::load($textid);
-                  $node->{$field_name}->value = $arg[$i][$field_name];
-                  $node->{$field_name}->format = 'full_html';
-
-                  $node->save();
+                if (!isset($arg[$i]['positional_index']) || !isset($arg[$i]['language'])) {
 
                   $message = [
-                    'success' => 1,
-                    'message' => 'content added',
+                    'success' => 0,
+                    'message' => 'required parameters missing(incorrect arguments)',
                   ];
-                  $statuscode = 200;
+                  $statuscode = 400;
+                  break;
 
                 }
+
                 else {
-                  // Create a text node.
-                  $str = explode('.', $arg[$i]['positional_index']);
-                  $chapter_name = 'Chapter ' . $str[0];
-                  $sloka_name = 'Sloka ' . $str[1];
 
-                  // $language_id = db_query('SELECT tid FROM `taxonomy_term_field_data` WHERE name = :language', [':language' => $arg[$i]['language']])->fetchField();
-                  // $arg[$i]['language'] = $language_id;
-                  $chapter_id = db_query("SELECT tid FROM `taxonomy_term_field_data` WHERE vid = :vid AND name = :name", [':vid' => 'gita', ':name' => $chapter_name])->fetchField();
+                  // Convert the language into language code.
+                  $languages = \Drupal::service('language_manager')->getLanguages(LanguageInterface::STATE_CONFIGURABLE);
 
-                  $sloka_id = db_query("SELECT tid FROM `taxonomy_term_field_data` WHERE vid = :vid AND name = :name AND tid IN (SELECT entity_id FROM `taxonomy_term__parent` WHERE parent_target_id = :parent_tid)", [':vid' => 'gita', ':name' => $sloka_name, ':parent_tid' => $chapter_id])->fetchField();
+                  foreach ($languages as $language) {
+                    if ($arg[$i]['language'] == $language->getName()) {
+                      $arg[$i]['language'] = $language->getId();
+                    }
 
-                  $node = entity_create('node', [
+                  }
 
-                    'type' => 'gita',
-                    'title' => $arg[$i]['positional_index'],
-                    'langcode' => $arg[$i]['language'],
+                  // $arg[$i]['language'] = "dv";
+                  // Make the db querries here.
+                  $textid = db_query("SELECT entity_id FROM `node__field_positional_index` WHERE langcode = :language AND field_positional_index_target_id IN (SELECT entity_id FROM `taxonomy_term__field_position` WHERE field_position_value = :positional_index)", ['language' => $arg[$i]['language'], ':positional_index' => $arg[$i]['positional_index']])->fetchField();
 
-                    'field_positional_index' => [['target_id' => (int) $chapter_id], ['target_id' => (int) $sloka_id]],
-                    $field_name => ['value' => $arg[$i][$field_name], 'format' => 'full_html'],
+                  if (isset($textid) && $textid > 0) {
+                    // Insert the content.
+                    $node = Node::load($textid);
+                    $node->{$field_name}->value = $arg[$i][$field_name];
+                    $node->{$field_name}->format = 'full_html';
 
-                  ]
-                    );
-                  $node->save();
+                    $node->save();
 
-                  $message = [
-                    'success' => 1,
-                    'message' => 'text created and content posted',
-                  ];
-                  $statuscode = 200;
+                    $message = [
+                      'success' => 1,
+                      'message' => 'content added',
+                    ];
+                    $statuscode = 200;
+
+                  }
+                  else {
+                    // Create a text node.
+                    $str = explode('.', $arg[$i]['positional_index']);
+                    $chapter_name = 'Chapter ' . $str[0];
+                    $sloka_name = 'Sloka ' . $str[1];
+
+                    // $language_id = db_query('SELECT tid FROM `taxonomy_term_field_data` WHERE name = :language', [':language' => $arg[$i]['language']])->fetchField();
+                    // $arg[$i]['language'] = $language_id;
+                    $chapter_id = db_query("SELECT tid FROM `taxonomy_term_field_data` WHERE vid = :vid AND name = :name", [':vid' => 'gita', ':name' => $chapter_name])->fetchField();
+
+                    $sloka_id = db_query("SELECT tid FROM `taxonomy_term_field_data` WHERE vid = :vid AND name = :name AND tid IN (SELECT entity_id FROM `taxonomy_term__parent` WHERE parent_target_id = :parent_tid)", [':vid' => 'gita', ':name' => $sloka_name, ':parent_tid' => $chapter_id])->fetchField();
+
+                    $node = entity_create('node', [
+
+                      'type' => 'gita',
+                      'title' => $arg[$i]['positional_index'],
+                      'langcode' => $arg[$i]['language'],
+
+                      'field_positional_index' => [['target_id' => (int) $chapter_id], ['target_id' => (int) $sloka_id]],
+                      $field_name => ['value' => $arg[$i][$field_name], 'format' => 'full_html'],
+
+                    ]
+                      );
+                    $node->save();
+
+                    $message = [
+                      'success' => 1,
+                      'message' => 'text created and content posted',
+                    ];
+                    $statuscode = 200;
+                  }
+                  // Else create a new node, to be done later.
                 }
-                // Else create a new node, to be done later.
               }
+
             }
+            else {
+              $message = [
+                'success' => 0,
+                'message' => 'source of the given id does not exist',
+              ];
 
+              $statuscode = 404;
+            }
           }
-          else {
-            $message = [
-              'success' => 0,
-              'message' => 'source of the given id does not exist',
-            ];
-
-            $statuscode = 404;
-          }
-
         }
+
       }
 
     }
